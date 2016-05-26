1c832496b43724d098cd9157157847da7108e2b9
George King <george.w.king@gmail.com>
2016-05-26 17:25:26 -0400

    websters-raw.txt carriage return line endings are now stripped.

--- websters-raw.txt.py
+++ websters-raw.txt.py
@@ -6,4 +6,8 @@ import muck
 
 # https currently fails with handshake error; stock py3 3.5.1 ships with 'OpenSSL 0.9.8zh 14 Jan 2016'.
 # http works fine.
-muck.fetch('http://www.gutenberg.org/ebooks/29765.txt.utf-8', path=sys.argv[1])
+file = muck.source_url('http://www.gutenberg.org/ebooks/29765.txt.utf-8')
+
+# read and then write out to get rid of '\r\n' line endings in the original.
+for line in file:
+  sys.stdout.write(line)
