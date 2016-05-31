97397478c1d72d3a7e2b343e4bf61d843ebee1b9
George King <george.w.king@gmail.com>
2016-05-31 15:14:31 -0400

    add explicit source extension to gutenberg source_url call.

--- websters-lines.txt.py
+++ websters-lines.txt.py
@@ -6,7 +6,7 @@ import muck
 
 # https currently fails with handshake error; stock py3 3.5.1 ships with 'OpenSSL 0.9.8zh 14 Jan 2016'.
 # http works fine.
-file = muck.source_url('http://www.gutenberg.org/ebooks/29765.txt.utf-8')
+file = muck.source_url('http://www.gutenberg.org/ebooks/29765.txt.utf-8', ext='.txt')
 
 # read and then write out to get rid of '\r\n' line endings in the original.
 for line in file:
