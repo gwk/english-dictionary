d22d9eec731368f6dcb33d70f1ff1eef2f44e11c
George King <george.w.king@gmail.com>
2016-10-15 15:09:36 -0400

    stopwords.json.py.

---
+++ stopwords.json.py
@@ -0,0 +1,8 @@
+# Generate list of stopwords.
+
+from nltk.corpus import stopwords
+from pithy.io import out_json
+
+words = stopwords.words('english') # Loading from NLTK corpus is a little slow (~0.4 seconds).
+
+out_json(words)
