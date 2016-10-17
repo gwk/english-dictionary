eb2dd91b0ca81a90ab54e5566d2380ec1410ae2a
Saurav <jain.saurav@hotmail.com>
2016-10-16 21:02:03 -0400

    added histogram feature

--- saurav.txt.py
+++ saurav.txt.py
@@ -2,7 +2,9 @@ import muck
 from collections import defaultdict
 import json
 import operator
-#import networkx as nx
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
 
 dictionary = muck.source('websters-basics.json')
 #print dictionary['Em']['words']
@@ -18,7 +20,7 @@ for item in dictionary:
 		defn_dict[defn] += 1
 top5 = dict(sorted(defn_dict.items(), key = operator.itemgetter(1), reverse = True)[:5])
 
-#g = nx.Graph()
+g = nx.DiGraph()
 node_list = []
 edge_list = []
 for item in dictionary:
@@ -26,8 +28,23 @@ for item in dictionary:
 	defns = dictionary[item]['defns']
 	for defn in defns:
 		edge_list.append((item, defn))
+		g.add_path([defn, item])
 
-print(len(node_list))
-#print(edge_list)
-print(top5)
-#print(freq_dict)
+scc_list = nx.strongly_connected_components(g)
+print(scc_list)
+scc_size = defaultdict(int)
+for scc in scc_list:
+	scc_size[len(list(scc))] += 1
+	'''
+	if len(list(scc)) > 5:
+		#print strongly connected components of length > 5
+		print(list(scc))
+	'''
+print(scc_size)
+#Print histogram of scc size distribution
+X = np.arange(len(scc_size))
+plt.bar(X, scc_size.values(),align = 'center', width = 0.5)
+plt.xticks(X, scc_size.keys())
+max_y = max(scc_size.values()) + 1
+plt.ylim(0, max_y)
+plt.show()
