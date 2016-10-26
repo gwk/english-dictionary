4c6fce05f6ef4d12b3723181804f2f9b90ed1c31
George King <george.w.king@gmail.com>
2016-10-26 17:50:58 -0400

    add index.html, word-sets-scc-sizes.csv.py steps.

---
+++ wb/word-sets-scc-sizes.csv.py
@@ -0,0 +1,21 @@
+# Analyze the largest strongly connected component.
+
+import muck
+
+from collections import Counter
+from operator import itemgetter
+from pithy.io import *
+from pithy.csv_utils import write_csv
+from networkx import strongly_connected_components as calc_sccs
+
+import graphs # registers .graph loader.
+
+graph = muck.load('wb/word-sets-graph.graph')
+
+sccs = list(calc_sccs(graph))
+errFL('sccs: {}', len(sccs))
+
+hist = Counter(len(scc) for scc in sccs)
+
+write_csv(stdout, ('SCC Size', 'Count'), sorted(hist.items(), key=itemgetter(1)))
+
