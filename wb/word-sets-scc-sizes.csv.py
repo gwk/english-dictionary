d2bca267952b30851532367848e4822d8d3c3f67
George King <george.w.king@gmail.com>
2016-11-01 15:07:28 -0400

    fix word-sets-scc-sizes.csv sort.

--- wb/word-sets-scc-sizes.csv.py
+++ wb/word-sets-scc-sizes.csv.py
@@ -17,5 +17,5 @@ errFL('sccs: {}', len(sccs))
 
 hist = Counter(len(scc) for scc in sccs)
 
-write_csv(stdout, ('SCC Size', 'Count'), sorted(hist.items(), key=itemgetter(1)))
+write_csv(stdout, ('SCC Size', 'Count'), sorted(hist.items(), reverse=True))
 
